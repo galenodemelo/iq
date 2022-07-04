9a97c11e553eab0864a362952dc649791b2471ff
Galeno de Melo <galenodemelo@gmail.com>
2022-07-04 20:34:57 -0300

    feat: On drag, move the switch
    # It can't be done with `onDrag` because of ghost images

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -44,6 +44,34 @@ export default class Switch extends Component<any, State> {
         this.bind()
     }
 
+    bind(): void {
+        document.addEventListener("mousedown", () => {
+            document.addEventListener("mousemove", this.moveHandler, { passive: true })
+        }, { passive: true })
+    }
+
+    moveHandler(event: MouseEvent): void {
+        const _this: Switch = window.switchComponent
+        if (!_this.switchObject || !_this.handlerObject) return
+
+        const handlerWidth: number = _this.handlerObject.offsetWidth
+        const switchWidth: number = _this.switchObject.offsetWidth
+        const padding: number = _this.state.switchPadding
+
+        const initialPosition: number = _this.switchObject.offsetLeft + padding
+        const distanceToDrag: number = switchWidth - padding * 2 - handlerWidth
+
+        let progress: number = event.clientX - initialPosition - handlerWidth / 2
+        if (progress < 0) progress = 0
+        else if (progress > distanceToDrag) progress = distanceToDrag
+
+        _this.setState({
+            distanceToDrag: distanceToDrag,
+            progress: progress
+        })
+
+        _this.handlerObject.style.left = `${_this.state.progress}px`
+    }
     render(): React.ReactNode {
         return (
             <div className={styles.switch} ref={this.switchRef}>
