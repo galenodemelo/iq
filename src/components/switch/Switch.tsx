c1c121648cbb4f534bf60a6877087c03811e2997
Galeno de Melo <galenodemelo@gmail.com>
2022-07-04 21:46:09 -0300

    feat: Adding gradient glow effect

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -15,12 +15,17 @@ declare global {
 
 export default class Switch extends Component<any, State> {
 
+    animationDurationInMilliseconds: number = 500
+    animationDurationInSeconds: number = this.animationDurationInMilliseconds / 1000
+
+    activeBackgroundRef
     handlerRef
     minDistanceToDragInPercentage: number = 75
     switchRef
 
     constructor(props: {} | Readonly<{}>) {
         super(props)
+        this.activeBackgroundRef = React.createRef<HTMLDivElement>()
         this.handlerRef = React.createRef<HTMLDivElement>()
         this.switchRef = React.createRef<HTMLDivElement>()
 
@@ -31,8 +36,9 @@ export default class Switch extends Component<any, State> {
         }
     }
 
-    get switchObject(): HTMLDivElement | null { return this.switchRef.current }
+    get activeBackgroundObject(): HTMLDivElement | null { return this.activeBackgroundRef.current }
     get handlerObject(): HTMLDivElement | null { return this.handlerRef.current }
+    get switchObject(): HTMLDivElement | null { return this.switchRef.current }
     get progressInPercentage(): number {
         return this.state.progress / this.state.distanceToDrag * 100
     }
@@ -40,8 +46,13 @@ export default class Switch extends Component<any, State> {
     componentDidMount(): void {
         if (!this.switchObject || !this.handlerObject) return
 
-        this.setState({
-            switchPadding: parseFloat(window.getComputedStyle(this.switchObject, null).getPropertyValue("padding-left"))
+        this.setState({ switchPadding: parseFloat(window.getComputedStyle(this.switchObject, null).getPropertyValue("padding-right")) }, () => {
+            if (!this.switchObject || !this.handlerObject) return
+
+            const handlerWidth: number = this.handlerObject.offsetWidth
+            const switchWidth: number = this.switchObject.offsetWidth
+            const padding: number = this.state.switchPadding
+            this.setState({distanceToDrag: switchWidth - padding * 2 - handlerWidth})
         })
 
         window.switchComponent = this
@@ -49,38 +60,43 @@ export default class Switch extends Component<any, State> {
     }
 
     bind(): void {
-        document.addEventListener("mousedown", () => {
-            document.addEventListener("mousemove", this.moveHandler, { passive: true })
-            document.addEventListener("mouseup", this.stopHandler, { passive: true })
+        this.switchObject?.addEventListener("mousedown", (event: MouseEvent) => {
+            this.followMouseMoving(event)
+            document.addEventListener("mousemove", this.followMouseMoving, { passive: true })
+            document.addEventListener("mouseup", this.stopMouseMoving, { passive: true })
         }, { passive: true })
     }
 
-    moveHandler(event: MouseEvent): void {
+    moveHandler(): void {
+        const _this: Switch = window.switchComponent
+        if (!_this.switchObject || !_this.handlerObject) return
+
+        if (_this.state.progress < 0) {
+            _this.setState({ progress: 0 })
+        } else if (_this.state.progress > _this.state.distanceToDrag) {
+            _this.setState({ progress: _this.state.distanceToDrag })
+        }
+
+        _this.handlerObject.style.left = `${_this.state.progress}px`
+        _this.updateActiveTransparency()
+    }
+
+    followMouseMoving(event: MouseEvent):void {
         const _this: Switch = window.switchComponent
         if (!_this.switchObject || !_this.handlerObject) return
 
         const handlerWidth: number = _this.handlerObject.offsetWidth
-        const switchWidth: number = _this.switchObject.offsetWidth
         const padding: number = _this.state.switchPadding
 
         const initialPosition: number = _this.switchObject.offsetLeft + padding
-        const distanceToDrag: number = switchWidth - padding * 2 - handlerWidth
-
         let progress: number = event.clientX - initialPosition - handlerWidth / 2
-        if (progress < 0) progress = 0
-        else if (progress > distanceToDrag) progress = distanceToDrag
-
-        _this.setState({
-            distanceToDrag: distanceToDrag,
-            progress: progress
-        })
-
-        _this.handlerObject.style.left = `${_this.state.progress}px`
+        _this.setState({progress: progress})
+        _this.moveHandler()
     }
 
-    stopHandler(): void {
+    stopMouseMoving(): void {
         const _this: Switch = window.switchComponent
-        document.removeEventListener("mousemove", _this.moveHandler)
+        document.removeEventListener("mousemove", _this.followMouseMoving)
 
         const reachedMinimumDistance: boolean = _this.progressInPercentage >= _this.minDistanceToDragInPercentage
         if (!reachedMinimumDistance) {
@@ -89,42 +105,50 @@ export default class Switch extends Component<any, State> {
             _this.moveHandlerToEnd()
         }
 
-        document.removeEventListener("mouseup", _this.stopHandler)
+        document.removeEventListener("mouseup", _this.stopMouseMoving)
     }
 
     moveHandlerToStart(): void {
-        if (!this.handlerObject) return
+        if (!this.handlerObject || !this.activeBackgroundObject) return
 
-        const animationDurationInMilliseconds: number = 500
-        const animationDurationInSeconds: number = 500 / 1000
-
-        this.handlerObject.style.transition = `left ${animationDurationInSeconds}s ease`
-        this.handlerObject.style.left = "0px"
+        this.handlerObject.style.transition = `left ${this.animationDurationInSeconds}s ease`
+        this.activeBackgroundObject.style.transition = `opacity ${this.animationDurationInSeconds}s ease`
+        this.setState({progress: 0})
+        this.moveHandler()
 
         setTimeout(() => {
-            if (!this.handlerObject) return
+            if (!this.handlerObject || !this.activeBackgroundObject) return
             this.handlerObject.style.transition = ""
-        }, animationDurationInMilliseconds)
+            this.activeBackgroundObject.style.transition = ""
+        }, this.animationDurationInMilliseconds)
     }
 
     moveHandlerToEnd(): void {
-        if (!this.handlerObject) return
+        if (!this.handlerObject || !this.activeBackgroundObject) return
 
-        const animationDurationInMilliseconds: number = 500
-        const animationDurationInSeconds: number = 500 / 1000
-
-        this.handlerObject.style.transition = `left ${animationDurationInSeconds}s ease`
-        this.handlerObject.style.left = `${this.state.distanceToDrag}px`
+        this.handlerObject.style.transition = `left ${this.animationDurationInSeconds}s ease`
+        this.activeBackgroundObject.style.transition = `opacity ${this.animationDurationInSeconds}s ease`
+        this.setState({progress: this.state.distanceToDrag})
+        this.moveHandler()
 
         setTimeout(() => {
-            if (!this.handlerObject) return
+            if (!this.handlerObject || !this.activeBackgroundObject) return
             this.handlerObject.style.transition = ""
-        }, animationDurationInMilliseconds)
+            this.activeBackgroundObject.style.transition = ""
+        }, this.animationDurationInMilliseconds)
+    }
+
+    updateActiveTransparency(): void {
+        const _this: Switch = window.switchComponent
+        if (!_this.activeBackgroundObject) return
+
+        _this.activeBackgroundObject.style.opacity = `${_this.progressInPercentage / 100}`
     }
 
     render(): React.ReactNode {
         return (
             <div className={styles.switch} ref={this.switchRef}>
+                <div className={styles.activeBackground} ref={this.activeBackgroundRef}></div>
                 <div className={styles.handler} ref={this.handlerRef}></div>
             </div>
         )
