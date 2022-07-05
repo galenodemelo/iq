89e6cf07e8c4d8fb25db15951b619803d08361f8
Galeno de Melo <galenodemelo@gmail.com>
2022-07-04 22:17:22 -0300

    feat: Adding "Powered on" and "Powered off" animations

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -17,16 +17,20 @@ export default class Switch extends Component<any, State> {
 
     animationDurationInMilliseconds: number = 500
     animationDurationInSeconds: number = this.animationDurationInMilliseconds / 1000
+    minDistanceToDragInPercentage: number = 75
 
     activeBackgroundRef
     handlerRef
-    minDistanceToDragInPercentage: number = 75
+    poweredOnRef
+    poweredOffRef
     switchRef
 
     constructor(props: {} | Readonly<{}>) {
         super(props)
         this.activeBackgroundRef = React.createRef<HTMLDivElement>()
         this.handlerRef = React.createRef<HTMLDivElement>()
+        this.poweredOnRef = React.createRef<HTMLDivElement>()
+        this.poweredOffRef = React.createRef<HTMLDivElement>()
         this.switchRef = React.createRef<HTMLDivElement>()
 
         this.state = {
@@ -38,6 +42,8 @@ export default class Switch extends Component<any, State> {
 
     get activeBackgroundObject(): HTMLDivElement | null { return this.activeBackgroundRef.current }
     get handlerObject(): HTMLDivElement | null { return this.handlerRef.current }
+    get poweredOnObject(): HTMLDivElement | null { return this.poweredOnRef.current }
+    get poweredOffObject(): HTMLDivElement | null { return this.poweredOffRef.current }
     get switchObject(): HTMLDivElement | null { return this.switchRef.current }
     get progressInPercentage(): number {
         return this.state.progress / this.state.distanceToDrag * 100
@@ -79,6 +85,7 @@ export default class Switch extends Component<any, State> {
 
         _this.handlerObject.style.left = `${_this.state.progress}px`
         _this.updateActiveTransparency()
+        _this.updatePoweredBoxes()
     }
 
     followMouseMoving(event: MouseEvent):void {
@@ -108,32 +115,28 @@ export default class Switch extends Component<any, State> {
     }
 
     moveHandlerToStart(): void {
-        if (!this.handlerObject || !this.activeBackgroundObject) return
-
-        this.handlerObject.style.transition = `left ${this.animationDurationInSeconds}s ease`
-        this.activeBackgroundObject.style.transition = `opacity ${this.animationDurationInSeconds}s ease`
+        this.dropHandlerAnimation()
         this.setState({progress: 0})
         this.moveHandler()
-
-        setTimeout(() => {
-            if (!this.handlerObject || !this.activeBackgroundObject) return
-            this.handlerObject.style.transition = ""
-            this.activeBackgroundObject.style.transition = ""
-        }, this.animationDurationInMilliseconds)
     }
 
     moveHandlerToEnd(): void {
-        if (!this.handlerObject || !this.activeBackgroundObject) return
-
-        this.handlerObject.style.transition = `left ${this.animationDurationInSeconds}s ease`
-        this.activeBackgroundObject.style.transition = `opacity ${this.animationDurationInSeconds}s ease`
+        this.dropHandlerAnimation()
         this.setState({progress: this.state.distanceToDrag})
         this.moveHandler()
+    }
+
+    dropHandlerAnimation() {
+        if (this.handlerObject) this.handlerObject.style.transition = `left ${this.animationDurationInSeconds}s ease`
+        if (this.activeBackgroundObject) this.activeBackgroundObject.style.transition = `opacity ${this.animationDurationInSeconds}s ease`
+        if (this.poweredOnObject) this.poweredOnObject.style.transition = `width ${this.animationDurationInSeconds}s ease`
+        if (this.poweredOffObject) this.poweredOffObject.style.transition = `width ${this.animationDurationInSeconds}s ease`
 
         setTimeout(() => {
-            if (!this.handlerObject || !this.activeBackgroundObject) return
-            this.handlerObject.style.transition = ""
-            this.activeBackgroundObject.style.transition = ""
+            if (this.handlerObject) this.handlerObject.style.transition = ""
+            if (this.activeBackgroundObject) this.activeBackgroundObject.style.transition = ""
+            if (this.poweredOnObject) this.poweredOnObject.style.transition = ""
+            if (this.poweredOffObject) this.poweredOffObject.style.transition = ""
         }, this.animationDurationInMilliseconds)
     }
 
@@ -144,10 +147,23 @@ export default class Switch extends Component<any, State> {
         _this.activeBackgroundObject.style.opacity = `${_this.progressInPercentage / 100}`
     }
 
+    updatePoweredBoxes(): void {
+        const _this: Switch = window.switchComponent
+        if (!_this.poweredOnObject || !_this.poweredOffObject) return
+
+        const poweredOnWidth: number = _this.progressInPercentage
+        const poweredOffWidth: number = 100 - _this.progressInPercentage
+
+        _this.poweredOnObject.style.width = `${poweredOnWidth}%`
+        _this.poweredOffObject.style.width = `${poweredOffWidth}%`
+    }
+
     render(): React.ReactNode {
         return (
             <div className={styles.switch} ref={this.switchRef}>
                 <div className={styles.activeBackground} ref={this.activeBackgroundRef}></div>
+                <div className={styles.poweredOn} ref={this.poweredOnRef}>Powered on</div>
+                <div className={styles.poweredOff} ref={this.poweredOffRef}>Powered off</div>
                 <div className={styles.handler} ref={this.handlerRef}></div>
             </div>
         )
