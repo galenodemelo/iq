7d75419bf1f32f83ba9cd1bb61e46ee6a368c4ad
Galeno de Melo <galenodemelo@gmail.com>
2022-07-16 11:17:42 -0300

    feat: Adding animated logo triggered on show

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { Component, ReactNode } from "react"
 import styles from "./Menu.module.sass"
 
@@ -15,6 +16,7 @@ export default class Menu extends Component<Props, State> {
     render(): ReactNode {
         return (
             <header className={styles.menu}>
+                <Image src="/img/iq-logo-animated.gif" key={1} width={300} height={108} layout="fixed" alt="IQ - Powered by people" />
             </header>
         )
     }
