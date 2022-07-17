eeb7704c8bd8bca5fdd845113b26a0020894a1f5
Galeno de Melo <galenodemelo@gmail.com>
2022-07-17 17:30:42 -0300

    feat: Adding success page

---
+++ src/layouts/contact/success/ContactSuccessLayout.tsx
@@ -0,0 +1,38 @@
+import GradientLettering from "@components/gradientlettering/GradientLettering"
+import MenuContact from "@components/menu/contact/MenuContact"
+import Image from "next/image"
+import { Component, ReactNode } from "react"
+import styles from "./ContactSuccessLayout.module.sass"
+
+export default class ContactSuccessLayout extends Component<any, any> {
+    render(): ReactNode {
+        return (
+            <main className={styles.contactSuccess}>
+                <MenuContact />
+
+                <div className={styles.content}>
+                    <h1 className={styles.lettering}>
+                        <span>You chose</span><br />
+                        <big>
+                            <GradientLettering>
+                                wisely!
+                            </GradientLettering>
+                        </big>
+                    </h1>
+
+                    <p className={styles.paragraph}>
+                        Thanks!<br />
+                        Our team will contact you shortly.
+                    </p>
+
+                    <p className={[styles.paragraph, styles.playlist].join(" ")}>
+                        in the meantime, enjoy what we're listening on <b>Spotify!</b>
+                        <a className={styles.icon}>
+                            <Image src="/img/ico-spotify.svg" width={48} height={48} alt="Spotify logo" />
+                        </a>
+                    </p>
+                </div>
+            </main>
+        )
+    }
+}
