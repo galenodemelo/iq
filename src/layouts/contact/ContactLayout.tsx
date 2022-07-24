2dcfada2bac353cec2e612538e46f78d35e4b564
Galeno de Melo <galenodemelo@gmail.com>
2022-07-24 20:28:05 -0300

    feat: Animating transition on open

--- src/layouts/contact/ContactLayout.tsx
+++ src/layouts/contact/ContactLayout.tsx
@@ -4,12 +4,20 @@ import React, { Component, ReactNode } from "react"
 import PoppingLetteringAnimation from "src/animations/PoppingLetteringAnimation"
 import styles from "./ContactLayout.module.sass"
 
-export default class ContactLayout extends Component {
+interface Props {
+    active: boolean
+}
 
+export default class ContactLayout extends Component<Props, any> {
+
+    closeRef
+    containerRef
     letteringRef
 
-    constructor(props: {}) {
+    constructor(props: Props) {
         super(props)
+        this.closeRef = React.createRef<HTMLAnchorElement>()
+        this.containerRef = React.createRef<HTMLElement>()
         this.letteringRef = React.createRef<HTMLHeadingElement>()
     }
 
@@ -17,12 +25,18 @@ export default class ContactLayout extends Component {
         const letteringAnimation: PoppingLetteringAnimation = new PoppingLetteringAnimation(this.letteringRef.current)
         letteringAnimation.fixGradientLettering(styles.gradient)
         letteringAnimation.run()
+
+        this.buildClipPath()
+    }
+
+    componentDidUpdate(): void {
+        this.buildClipPath()
     }
 
     render(): ReactNode {
         return (
-            <main className={styles.contact}>
-                <MenuContact />
+            <main className={styles.contact} data-active={this.props.active} ref={this.containerRef}>
+                <MenuContact closeButtonRef={this.closeRef} />
 
                 <div className={styles.content}>
                     <h1 className={styles.lettering} ref={this.letteringRef}>
@@ -32,7 +46,7 @@ export default class ContactLayout extends Component {
                         <div className={styles.gradient}>future</div>
                     </h1>
 
-                    <div className={styles.contact}>
+                    <div className={styles.form}>
                         <ContactForm />
 
                         <footer className={styles.footer}>
@@ -42,5 +56,17 @@ export default class ContactLayout extends Component {
                 </div>
             </main>
         )
+    }
+
+    private buildClipPath(): void {
+        if (this.props.active) {
+            this.containerRef.current!.style.clipPath = "circle(100%)"
+            return
+        }
+
+        const closeButtonPosition = this.closeRef.current!.getBoundingClientRect()
+        const xPosition: number = closeButtonPosition.x + closeButtonPosition.width
+        const yPosition: number = closeButtonPosition.y + closeButtonPosition.height
+        this.containerRef.current!.style.clipPath = `circle(0% at ${xPosition}px ${yPosition}px)`
     }
 }
