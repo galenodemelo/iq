f2dffaa08b1b382ee6312e83c66c02b94e8fa720
Galeno de Melo <galenodemelo@gmail.com>
2022-07-27 20:23:46 -0300

    feat: Validating data server-side

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -7,8 +7,43 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
             return
         }
 
+        const requestData: ContactRequestDTO = new ContactRequestDTO(req.body.fullName, req.body.email, req.body.phone, req.body.recaptchaToken)
+        const errorList: Array<string> = await validateRequestData(requestData)
+        if (errorList.length > 0) {
+            res.status(422).json({success: false, errorList: errorList})
+            return
+        }
     } catch (error) {
         console.error(`api/contact >> ${JSON.stringify(req.body)}`, error)
         res.status(500).json({success: false, errorList: ["Internal server error"]})
     }
+}
+
+class ContactRequestDTO {
+    fullName: string
+    email: string
+    phone: string
+    recaptchaToken: string
+
+    constructor(fullName: string, email: string, phone: string, recaptchaToken: string) {
+        this.fullName = fullName.trim()
+        this.email = email.trim()
+        this.phone = phone.trim()
+        this.recaptchaToken = recaptchaToken.trim()
+    }
+}
+
+async function validateRequestData(requestData: ContactRequestDTO): Promise<Array<string>> {
+    let errorList: Array<string> = []
+
+    if (!requestData.fullName.length) errorList.push("Please inform your name")
+
+    const isValidEmail: boolean = requestData.email.length > 0 && /\S+@\S+\.\S+/.test(requestData.email)
+    if (!isValidEmail) errorList.push("Please inform a valid email")
+
+    if (!requestData.phone.length) errorList.push("Please inform your phone number")
+
+    if (!requestData.recaptchaToken.length) errorList.push("Missing ReCAPTCHA Token")
+
+    return errorList
 }
--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { Component, ReactNode } from "react"
+import React, { Component, ReactNode } from "react"
 import styles from "./ContactForm.module.sass"
 
 type State = {
@@ -8,16 +8,37 @@ type State = {
 
 export default class ContactForm extends Component<any, State> {
 
+    formReference: React.RefObject<HTMLFormElement>
     constructor(props: {}) {
         super(props)
+        this.formReference = React.createRef<HTMLFormElement>()
+
         this.state = { isSubmiting: false }
         this.send = this.send.bind(this)
     }
 
     async send(): Promise<void> {
+        if (!this.formReference.current) return
         try {
             this.setState({ isSubmiting: true })
-    }
+            const formData: FormData = new FormData(this.formReference.current)
+            const formAction: string = this.formReference.current.getAttribute("action")!.toString()
+            const formMethod: string = this.formReference.current.getAttribute("method")!.toString()
+            const formDataAsJson: string = JSON.stringify(Object.fromEntries(formData))
+
+            const response: Response = await fetch(formAction, {
+                method: formMethod,
+                headers: new Headers({"Content-Type": "application/json"}),
+                body: formDataAsJson
+            })
+            const responseJson: { success: boolean, errorList?: Array<string>, message?: string } = await response.json()
+            if (!responseJson.success) {
+                alert(responseJson.errorList?.join("\n"))
+                return
+            }
+
+            alert(responseJson.message)
+            this.formReference.current.reset()
         } catch (error) {
             console.error(error)
             alert("An unexpected error ocurred. Please, contact the support")
@@ -28,7 +49,7 @@ export default class ContactForm extends Component<any, State> {
 
     render(): ReactNode {
         return (
-            <form className={styles.contactForm} action="/api/contact/contact" method="POST" autoComplete="off">
+            <form className={styles.contactForm} action="/api/contact" method="POST" autoComplete="off" ref={this.formReference}>
                 <div className={styles.field}>
                     <input type="text" name="fullName" placeholder="Full Name" aria-label="Full Name" required />
                 </div>
